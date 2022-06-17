60e156e926b6e90db3802ee3ba05f5cedb7a6543
Dabsia <Dabojohnson98@gmail.com>
2022-06-16 22:35:02 -0700

    Started learning the datatypes of Typescript

---
+++ basics.ts
@@ -0,0 +1,15 @@
+// Objects in Typescript
+
+const person = {
+    name: 'Dabo',
+    age: 20,
+    hobbies: ['Football', 'Coding']
+};
+
+for (const hubby of person.hobbies) {
+    console.log(hubby.toLocaleUpperCase())
+}
+
+let favoriteActivities : string[ ]
+
+console.log(person)
